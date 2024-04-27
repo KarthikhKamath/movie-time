2a8009d40bcf4ab0844ecc35b23048a5fd50e985
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2024-04-27 12:53:12 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <div className="infoText">
+                    Made with ❤️ by Karthik
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
